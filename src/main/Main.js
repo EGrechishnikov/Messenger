8ab48edc020d75fa352546364343820507741195
EGrehishnikov <ke6a93@gmail.com>
2019-01-14 17:33:04 +0300

    WebSocket connection

--- src/main/Main.js
+++ src/main/Main.js
@@ -8,23 +8,12 @@ class Main extends React.Component {
         // httpGet('user/admin/exist').then((response) => {
         //     console.log(response);
         // });
-        var socket = new SockJS('http://localhost:8080/secured/room');
+        var socket = new SockJS('http://localhost:8080/chat');
         var stompClient = Stomp.over(socket);
-        var sessionId;
 
-        stompClient.connect({}, function (frame) {
-            var url = stompClient.ws._transport.url;
-            url = url.replace(
-                "ws://localhost:8080/secured/room/",  "");
-            url = url.replace("/websocket", "");
-            url = url.replace(/^[0-9]+\//, "");
-            console.log("Your current session is: " + url);
-            sessionId = url;
-
-            stompClient.subscribe("/secured/history", (response) => {console.log(response.body);});
-            stompClient.subscribe("/secured/user/login/secured/user/queue/specific-user", () => {console.log(1);});
-            stompClient.send("/spring-security-mvc-socket/secured/chat", {}, 'it works!');
-            stompClient.send("/spring-security-mvc-socket/secured/room", {}, 'personal!');
+        stompClient.connect({}, ()=> {
+            stompClient.subscribe("/user/chat", (response) => {console.log(response.body);});
+            stompClient.send("/chat/message", {}, 'test');
         });
     }
 
@@ -38,9 +27,6 @@ class Main extends React.Component {
                 <h1>
                     Main
                 </h1>
-                {/*<SockJsClient url='http://localhost:8080/secured/room' topics={['']}*/}
-                              {/*onMessage={(msg) => { console.log(msg); }}*/}
-                              {/*ref={ (client) => { this.clientRef = client }} />*/}
                 <button onClick={this.test}>Test</button>
                 <button onClick={this.doLogout}>Logout</button>
             </div>
